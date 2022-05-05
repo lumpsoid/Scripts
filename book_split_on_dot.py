32015522048abc22ef1a4905c63d525512ffbe3b
lumpsoid <mmgeeki@gmail.com>
2022-05-05 19:23:28 +0300

    add book split on dot v1

--- book_split_on_dot.py
+++ book_split_on_dot.py
@@ -15,20 +15,38 @@ with open(path_to_book, 'r', encoding='utf-8') as file:
 
 splited_book_sentences = []
 for i in book_sentences:
-    splited_book_sentences.extend(i.split())
+    splited_book_sentences.extend(list(map(lambda x: x+'.\n', i.split('.'))))
 
-n = 3  # how much words in a row
+n = 50  # how much sentences in a chunk
 book_parts = [splited_book_sentences[i:i+n] for i in range(0, len(splited_book_sentences), n)]
-three_words = []
-for i in book_parts:
-    three_words.append(' '.join(i))
-three_words = list(map(lambda x: x + '\n', three_words))
+chapters = {}
 
-book_id = datetime.now()
-with codecs.open(f'/Users/qq/Dropbox/Obsidian/{book_id.strftime("%Y%m%d%H%M%S")}.md', 'w', encoding='utf-8') as file:
-    file.write(''.join(three_words))
+# inserting serial number of a chunk at top of it
+# and links to previous and next in bottom of a file
+for i in range(len(book_parts)):
+    now = datetime.now() + timedelta(seconds=i)
+    chapters.update({f'{name_of_book} {i}': now.strftime("%Y%m%d%H%M%S")})
+    book_parts[i].insert(0, f'{name_of_book} {i}')
+    book_parts[i].extend(['\n', '\n', '\n', f'{name_of_book} {i-1}', f'{name_of_book} {i+1}'])
+for parts in book_parts:
+    for i in range(len(parts)):
+        if not re.search(f'{name_of_book}', parts[i]) is None:
+            parts[i] = parts[i] + f' [[{chapters.get(parts[i])}]]\n'  # adding links
+toc_file = [f'- [ ] {k} [[{v}]]\n' for k,v in chapters.items()]
+toc_file.insert(0, f'{name_of_book}\n')  # adding header to the toc file with name of the book
+toc_file.extend(['\n', '\n', '\n', 'книги [[20220227220307]]'])  # adding link to the book zettel
 
-#modifing Book zettel with link to toc file of the book
+#export
+for i in range(len(book_parts)):
+    file_id = re.search('[0-9]{14}', book_parts[i][0])
+    if file_id:
+        with codecs.open(f'/Users/qq/Dropbox/Obsidian/test/{file_id.group(0)}.md', 'w', encoding='utf-8') as file:
+            file.write(''.join(book_parts[i]))
+toc_file_id = datetime.now() - timedelta(seconds=3)
+with codecs.open(f'/Users/qq/Dropbox/Obsidian/test/{toc_file_id.strftime("%Y%m%d%H%M%S")}.md', 'w', encoding='utf-8') as file:
+        file.write(''.join(toc_file))
+
+#modifing book zettel with link to toc file of the book
 book_zettel = []
 with open(path_to_book_zettel, 'r', encoding='utf-8') as file:
     book_zettel.extend(file)
@@ -37,34 +55,7 @@ for i in range(len(book_zettel)):
     if not re.search(r'- ', book_zettel[i]) is None:
         line_to_insert = i + 1
 new_book_zettel = book_zettel[:line_to_insert]
-new_book_zettel.append(f'- {name_of_book} [[{book_id.strftime("%Y%m%d%H%M%S")}]]\n')
+new_book_zettel.append(f'- {name_of_book} [[{toc_file_id.strftime("%Y%m%d%H%M%S")}]]\n')
 new_book_zettel.extend(book_zettel[line_to_insert:])
 with codecs.open(path_to_book_zettel, 'w', encoding='utf-8') as file:
     file.write(''.join(new_book_zettel))
-
-# # inserting serial number of a chunk at top of it
-# # and links to previous and next in bottom of a file
-# for i in range(len(book_parts)):
-#     now = datetime.now() + timedelta(seconds=i)
-#     chapters.update({f'{name_of_book} {i}': now.strftime("%Y%m%d%H%M%S")})
-#     book_parts[i].insert(0, f'{name_of_book} {i}')
-#     book_parts[i].extend(['\n', '\n', '\n', f'{name_of_book} {i-1}', f'{name_of_book} {i+1}'])
-# for parts in book_parts:
-#     for i in range(len(parts)):
-#         if not re.search(f'{name_of_book}', parts[i]) is None:
-#             parts[i] = parts[i] + f' [[{chapters.get(parts[i])}]]\n'  # adding links
-# toc_file = [f'- [ ] {k} [[{v}]]\n' for k,v in chapters.items()]
-# toc_file.insert(0, f'{name_of_book}\n')  # adding header to the toc file with name of the book
-# toc_file.extend(['\n', '\n', '\n', 'Книги [[20220227220307]]'])  # adding link to the Book zettel
-
-# #export
-# for i in range(len(book_parts)):
-#     file_id = re.search('[0-9]{14}', book_parts[i][0])
-#     if file_id:
-#         with codecs.open(f'/Users/qq/Dropbox/Obsidian/test/{file_id.group(0)}.md', 'w', encoding='utf-8') as file:
-#             file.write(''.join(book_parts[i]))
-# toc_file_id = datetime.now() - timedelta(seconds=3)
-# with codecs.open(f'/Users/qq/Dropbox/Obsidian/test/{toc_file_id.strftime("%Y%m%d%H%M%S")}.md', 'w', encoding='utf-8') as file:
-#         file.write(''.join(toc_file))
-
-
